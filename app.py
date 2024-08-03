2e8659207349e284b13757c8271f66906abba107
Keshav V <keshavvarma899@gmail.com>
2024-08-03 00:52:37 -0400

    mhs

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ MODEL_NAME = "gpt-35-turbo"
 # Set up the client for AI Chat  
 client = AzureOpenAI(api_key=AOAI_KEY,azure_endpoint=AOAI_ENDPOINT,api_version="2024-05-01-preview")  
   
-SCHEDULE_PROMPT = "You are a helpful schedule creator that creates schedules based on tasks created in a bulletpoint and organized format"
+SCHEDULE_PROMPT = "You are a helpful schedule creator that creates schedules based on tasks. List out these schedules with bullet points. Make sure to write each task on a new line with the time slot preceeding the task. Makse sure that the schedule reduces stress and overworking. "
   
 # Function to get AI response with context  
 def get_schedule(question, chat_history):  
@@ -22,21 +22,16 @@ def get_schedule(question, chat_history):
     messages.extend(chat_history)  
     messages.append({"role": "user", "content": question})  
   
-    # Get the answer using the GPT model (create 1 answer (n) and use a temperature of 0.7 to set it to be pretty creative/random)
     response = client.chat.completions.create(  
         model=MODEL_NAME,  
-        temperature=0.7,  
+        temperature=0.2,  
         n=1,  
         messages=messages,  
     )  
     answer = response.choices[0].message.content  
-    #print(answer)
     return answer  
   
-############################################
-######## THIS IS THE WEB APP CODE  #########
-############################################
-# Create a flask app
+
 app = Flask(__name__, template_folder='templates', static_folder='static')  
   
 # Routes  
@@ -69,6 +64,33 @@ def chat():
         return get_schedule(question, chat_history)  
     else:  
         return render_template("schedule.html")  
+    
+THERAPY_PROMPT = "You are a mental health support chatbot that provides support to users. You should provide empathetic responses to users and help them feel better. You should also provide resources to help them get the support they need. "
+
+def get_support(question, chat_history):  
+    # Create the message history  
+    messages = [{"role": "system", "content": THERAPY_PROMPT }]  
+    messages.extend(chat_history)  
+    messages.append({"role": "user", "content": question})  
+  
+    response = client.chat.completions.create(  
+        model=MODEL_NAME,  
+        temperature=0.2,  
+        n=1,  
+        messages=messages,  
+    )  
+    answer = response.choices[0].message.content  
+    return answer  
+  
+
+@app.route('/mental-health-support',methods=['GET', 'POST'])  
+def mhsupport():  
+    if request.method == 'POST':  
+        question = request.form.get("question")  
+        chat_history = request.form.get("chat_history", [])  
+        return get_support(question, chat_history)  
+    else:  
+        return render_template("mhs.html")  
   
 @app.errorhandler(404)  
 def handle_404(e):  
